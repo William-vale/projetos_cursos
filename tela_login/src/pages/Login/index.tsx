e21b1585256e8820c721b3e75e379d4244c1d22e
William-vale <william_vale@alu.ufc.br>
2024-06-10 17:42:34 -0300

    Projeto novo para os projetos de curso - primeiro commit

---
+++ tela_login/src/pages/Login/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Container } from './style';
+
+const Login: React.FC = () => 
+    <Container>
+        
+    </ Container>;
+
+export default Login;
---
+++ tela_login/src/pages/Login/style.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const Container = styled.login`
+    width: 200px;
+`;
