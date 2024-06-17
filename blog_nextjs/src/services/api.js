b00b510fc21493bd51dce32b922ba4bd61d19236
William-vale <william_vale@alu.ufc.br>
2024-06-17 20:25:44 -0300

    enxugando o projeto

--- blog_nextjs/src/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const api  = axios.create({
-    baseURL: 'https://hhsmmcacnjlginujjzgb.supabase.co/rest/v1/',
-    headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhoc21tY2FjbmpsZ2ludWpqemdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg2NjM3NDYsImV4cCI6MjAzNDIzOTc0Nn0.AtmJcJFlMTPtGdO_00wwRiiieZ0ysvqA_8-NAjF7upc",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhoc21tY2FjbmpsZ2ludWpqemdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg2NjM3NDYsImV4cCI6MjAzNDIzOTc0Nn0.AtmJcJFlMTPtGdO_00wwRiiieZ0ysvqA_8-NAjF7upc"
-    }
-})
